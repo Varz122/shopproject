ac8a265e90800330b7dc61951e0e22ffad98ed47
Varz122 <svarshini427>
2024-04-22 20:11:48 +0530

    Add new file to folder

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart = require('../models/cart');
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -10,13 +10,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getproduct = (req,res,next) =>{
-const prodId = req.params.productId
-Product.findById(prodId,product =>{
-console.log(product)
-})
-res.redirect('/')
-}
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
@@ -32,6 +36,13 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+exports.postCart = (req,res,next)=>{
+  const prodId = req.body.productId
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -10,9 +10,10 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/products/:productId',shopController.getproduct)
+router.get('/products/:productId',shopController.getProduct)
 
 router.get('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart)
 
 router.get('/orders', shopController.getOrders);
 
