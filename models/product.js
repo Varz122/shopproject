b1369142456187cf1895a7d5bbb98f4b149ac400
Varz122 <svarshini427>
2024-05-04 21:53:15 +0530

    sequelize

--- models/product.js
+++ models/product.js
@@ -1,27 +1,26 @@
-
-
-const db = require('../util/database')
-
-module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    return db.execute('INSERT INTO products (title,description,price,imageurl) VALUES (?,?,?,?)',
-    [this.title, this.description, this.price, this.imageUrl]
-  )
-    
-  }
-
-  static fetchAll() {
-   return db.execute('SELECT * FROM products')
-  }
-  static findById (id){
-    return db.execute('SELECT * FROM products WHERE id = ?', [id])
-  }
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database')
+const Product = sequelize.define('product',{
+  id :{
+    type : Sequelize.INTEGER,
+    autoIncrement : true,
+    allowNull : false,
+    primaryKey : true
+  },
+  title : Sequelize.STRING,
+  price : {
+    type : Sequelize.INTEGER,
+    allowNull : false
+  },
+  imageUrl : {
+    type : Sequelize.STRING,
+      allowNull : false
+    },
+   description : {
+    type : Sequelize.STRING,
+      allowNull : false
+   }
   
-};
+});
+module.exports = Product
+
